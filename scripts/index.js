ac1b81269f3aa60a7702d6a578122c1a7aaec8ec
Sh1zok <borovojarsenij218@gmail.com>
2025-12-04 22:03:25 +0300

    Completely Reworked all this shitty shit Signed-off-by: Sh1zok <borovojarsenij218@gmail.com>

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const WelcomeSplashes = [
+    "Hey there! I’m <a id='Sh1zok'>Sh1zok</a>!",
+    "Greetings! My name is <a id='Sh1zok'>Sh1zok</a>!",
+    "Yo! <a id='Sh1zok'>Sh1zok</a> is here!",
+    "Hello! Call me <a id='Sh1zok'>Sh1zok</a>.",
+    "Howdy! Ny name is <a id='Sh1zok'>Sh1zok</a>!",
+    "Welcome, my name is <a id='Sh1zok'>Sh1zok</a>!",
+    "Hiya! <a id='Sh1zok'>Sh1zok</a> is my name!",
+    "Nice to see you! I’m <a id='Sh1zok'>Sh1zok</a>!",
+    "Sup! I go by <a id='Sh1zok'>Sh1zok</a>!",
+    "Hi there, my nickname is <a id='Sh1zok'>Sh1zok</a>!",
+    "Welcome aboard! I’m <a id='Sh1zok'>Sh1zok</a>!",
+    "Cheers! My name is <a id='Sh1zok'>Sh1zok</a>!",
+    "What's up? I'm <a id='Sh1zok'>Sh1zok</a>!",
+    "I'm <a id='Sh1zok'>Sh1zok</a>. That's my website.",
+    "<a id='Sh1zok'>Sh1zok</a> was here."
+]
